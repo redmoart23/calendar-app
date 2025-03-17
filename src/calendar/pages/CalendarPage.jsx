ee9e63cd7ebb5c25b871d1285353ffd9cc0b1e0b
red <redmoart@gmail.com>
2025-03-17 10:43:10 -0500

    ✨Add user-specific event styling in CalendarPage; implement evenStyleGetter for personalized event display

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -8,16 +8,36 @@ import { localizer } from "../../helpers/calendarLocalizer";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import { getMessagesEs } from "../../helpers/getMessages";
 import { CalendarEvent } from "../components/CalendarEvent";
-import { useUiStore, useCalendarStore } from "../../hooks";
+import { useUiStore, useCalendarStore, useAuthStore } from "../../hooks";
 import { FabAddNew } from "../components/FabAddNew";
 import { FabDelete } from "../components/FabDelete";
 
 export const CalendarPage = () => {
+
+  const {user} = useAuthStore();
+
   const { openDateModal } = useUiStore();
   const { events, setActiveEvent , startLoadingEvents} = useCalendarStore();
   const [lastView, setLastView] = useState(localStorage.getItem("lastView")  || "week");
   const [laguage, setLaguage] = useState(false);
   const [currentDate, setCurrentDate] = useState(new Date());
+
+  // eslint-disable-next-line no-unused-vars
+  const evenStyleGetter = (event, start, end, isSelected) => {
+    
+    const isMyEvent = (user.uid === event.user._id) ||  (user.uid === event.user.uid)
+    
+    const style = {
+      backgroundColor: isMyEvent ? "#347CF7" : "#465660",
+      borderRadius: "0px",
+      opacity: 0.8,
+      color: "white",
+    }
+
+    return {
+      style
+    }
+  };
 
   const onChangeLanguage = () => {
     setLaguage((current) => !current);
@@ -39,6 +59,7 @@ export const CalendarPage = () => {
 
   useEffect(() => {
     startLoadingEvents();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
@@ -59,6 +80,7 @@ export const CalendarPage = () => {
           event: CalendarEvent,
         }}
         style={{ height: "calc(100vh - 80px)", width: "100%" }}
+        styleEvent={evenStyleGetter}
         onDoubleClickEvent={onDoubleClick}
         onSelectEvent={onSelectEvent}
         onView={onViewChange}
